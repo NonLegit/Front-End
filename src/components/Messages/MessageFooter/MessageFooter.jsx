ba6fc5b10e5ffa4cca998cf9fc7f00d6539940e0
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-10-25 13:44:59 +0200

    Add Message footer

--- src/components/Messages/MessageFooter/MessageFooter.jsx
+++ src/components/Messages/MessageFooter/MessageFooter.jsx
@@ -1,10 +1,82 @@
-import { Links } from './styles';
+import {
+  Footer, HeaderLinks, ContainerLinks, Links, EndFooter, FooterLinks,
+} from './styles';
 
 function MessageFooter() {
   return (
-    <Links href="https://www.redditinc.com/blog">
-      blog
-    </Links>
+    <Footer>
+      <FooterLinks>
+        <ContainerLinks first="true">
+          <HeaderLinks>about</HeaderLinks>
+          <Links href="https://www.redditinc.com/blog">
+            blog
+          </Links>
+          <Links href="https://www.redditinc.com/">
+            about
+          </Links>
+          <Links href="https://www.redditforbusiness.com/">
+            advertising
+          </Links>
+          <Links href="https://www.redditinc.com/careers">
+            careers
+          </Links>
+        </ContainerLinks>
+        <ContainerLinks>
+          <HeaderLinks>help</HeaderLinks>
+          <Links href="https://www.redditinc.com/policies/content-policy">
+            site rules
+          </Links>
+          <Links href="https://www.reddithelp.com/hc/en-us">
+            Reddit help center
+          </Links>
+          <Links href="https://www.reddit.com/wiki/reddiquette/">
+            reddiquette
+          </Links>
+          <Links href="https://www.redditinc.com/policies/moderator-code-of-conduct">
+            mod guidelines
+          </Links>
+          <Links href="https://www.reddit.com/contact/">
+            contact us
+          </Links>
+        </ContainerLinks>
+        <ContainerLinks>
+          <HeaderLinks>apps & tools</HeaderLinks>
+          <Links href="https://www.redditinc.com/blog">
+            Reddit for iPhone
+          </Links>
+          <Links href="https://www.redditinc.com/">
+            Reddit for Android
+          </Links>
+          <Links href="https://www.redditforbusiness.com/">
+            mobile website
+          </Links>
+        </ContainerLinks>
+        <ContainerLinks>
+          <HeaderLinks>{'<3'}</HeaderLinks>
+          <Links href="https://www.reddit.com/premium/">
+            Reddit premium
+          </Links>
+          <Links href="https://www.reddit.com/coins/">
+            Reddit coins
+          </Links>
+        </ContainerLinks>
+      </FooterLinks>
+      <EndFooter>
+        Use of this site constitutes acceptance of our
+        {' '}
+        <Links location="End" href="https://www.reddit.com/help/useragreement">User Agreement</Links>
+        {' '}
+        and
+        {' '}
+        <Links location="End" href="https://www.reddit.com/help/privacypolicy">Privacy Policy</Links>
+        {' '}
+        .
+        © 2022 reddit inc. All rights reserved.
+      </EndFooter>
+      <EndFooter>
+        REDDIT and the ALIEN Logo are registered trademarks of reddit inc.
+      </EndFooter>
+    </Footer>
   );
 }
 
--- src/components/Messages/MessageFooter/styles.js
+++ src/components/Messages/MessageFooter/styles.js
@@ -1,21 +1,45 @@
 import styled from '@emotion/styled';
 import { Link } from '@mui/material';
 
-const Links = styled(Link)(({ theme }) => ({
-  color: theme.palette.primary.main,
+const Footer = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '40px 0px 24px',
+}));
+const FooterLinks = styled('div')(() => ({
+  display: 'flex',
+  margin: '15px auto',
+  maxWidth: '600px',
+}));
+const Links = styled(Link)(({ location, theme }) => ({
   textDecoration: 'none',
-  fontSize: 'small',
+  fontSize: location === 'End' ? 'x-small' : 'small',
+  width: 'fit-content',
+  color: location === 'End' ? 'grey' : theme.palette.primary.main,
+  '&:visited': {
+    color: '#80bce9',
+  },
   '&:hover': {
     textDecoration: 'underline',
   },
-  '&:active': {
-    color: theme.palette.primary.light,
-  },
 }));
-const HeadLinks = styled(Link)(({ theme }) => ({
-  color: theme.palette.primary.main,
-
+const HeaderLinks = styled('span')(() => ({
+  color: '#777',
+  marginBottom: '5px',
+}));
+const ContainerLinks = styled('div')(({ theme, first }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  borderLeft: first === 'true' ? '' : '1px solid',
+  borderColor: theme.palette.grey[300],
+  padding: '0 15px',
+  marginBottom: '18px',
+}));
+const EndFooter = styled('p')(() => ({
+  color: 'grey',
+  fontSize: 'x-small',
+  margin: '0px auto',
 }));
 export {
-  Links, HeadLinks,
+  Footer, Links, ContainerLinks, HeaderLinks, EndFooter, FooterLinks,
 };
