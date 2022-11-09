acbf0eb4dfbfc8badbb79be369b9733645d21b23
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2022-11-09 12:56:45 +0200

    test: testing the create post page components

--- src/components/CreatePost/PostTypes/PostTypes.jsx
+++ src/components/CreatePost/PostTypes/PostTypes.jsx
@@ -19,11 +19,41 @@ function PostTypes(props) {
       value={postType}
       onChange={handlePostType}
     >
-      <CustomTab icon={<PostAddOutlinedIcon />} iconPosition="start" label="post" value={0} />
-      <Divider orientation="vertical" flexItem light />
-      <CustomTab icon={<ImageOutlinedIcon />} iconPosition="start" label="images & videos" value={1} />
-      <Divider orientation="vertical" flexItem light />
-      <CustomTab icon={<AttachFileIcon sx={{ transform: 'rotate(45deg)' }} />} iconPosition="start" label="link" value={2} />
+      <CustomTab
+        icon={<PostAddOutlinedIcon />}
+        iconPosition="start"
+        label="post"
+        value={0}
+        data-testid="text"
+      />
+      <Divider
+        orientation="vertical"
+        flexItem
+        light
+      />
+      <CustomTab
+        icon={<ImageOutlinedIcon />}
+        iconPosition="start"
+        label="images & videos"
+        value={1}
+        data-testid="media"
+      />
+      <Divider
+        orientation="vertical"
+        flexItem
+        light
+      />
+      <CustomTab
+        icon={(
+          <AttachFileIcon
+            sx={{ transform: 'rotate(45deg)' }}
+          />
+)}
+        iconPosition="start"
+        label="link"
+        value={2}
+        data-testid="url"
+      />
     </CustomTabs>
   );
 }
