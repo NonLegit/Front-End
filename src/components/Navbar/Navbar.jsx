eff6d3144ed7fddbe0fd5f5f908cc031cc598ed7
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-11-10 09:39:08 +0200

    Adding Navbar with responsive components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,34 @@
+/* eslint-disable no-unused-vars */
+import { Box } from '@mui/material';
+import MuiToolbar from '@mui/material/toolbar';
+import StyledNavbar from './styles';
+import HomeList from './HomeList/HomeList';
+import UserList from './UserList/UserList';
+import SearchButton from './Search/Search';
+import NavIcons from './NavIcons/NavIcons';
+import LogoIcon from './Logo/logoIcon';
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <StyledNavbar>
+      <MuiToolbar>
+        <Box sx={{
+          display: 'flex', width: '100%', alignItems: 'center', flexGrow: 1,
+        }}
+        >
+          <LogoIcon />
+          <HomeList />
+          <SearchButton />
+        </Box>
+        <Box sx={{
+          display: 'flex', alignItems: 'center', justifyContent: 'flex-end',
+        }}
+        >
+          <NavIcons />
+          <UserList />
+        </Box>
+      </MuiToolbar>
+    </StyledNavbar>
   );
 }
 
