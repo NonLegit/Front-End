c653d75baa54870d51e6330ca7d8c4488e9f8ee7
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2022-11-12 03:42:52 +0200

    Merge fix subreddit

--- src/components/Navbar/NavIcons/NavIcons.test.jsx
+++ src/components/Navbar/NavIcons/NavIcons.test.jsx
@@ -1,7 +1,10 @@
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavIcons from './NavIcons';
 
 test('test snapshot', async () => {
-  const tree = renderer.create(<NavIcons />).toJSON();
+  const tree = renderer.create(
+    <Router><NavIcons /></Router>,
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/Navbar/Navbar.test.jsx
+++ src/components/Navbar/Navbar.test.jsx
@@ -1,7 +1,10 @@
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 
 test('test snapshot', async () => {
-  const tree = renderer.create(<Navbar />).toJSON();
+  const tree = renderer.create(
+    <Router><Navbar /></Router>,
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
