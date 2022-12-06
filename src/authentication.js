08b7ef807aa8d4fee64f3814734029b28bc62aa2
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-06 20:58:40 +0200

    fix update cookies

--- src/authentication.js
+++ src/authentication.js
@@ -1,7 +1,10 @@
 import { useCookies } from 'react-cookie';
+import { redditCookie } from './components/Authentication/authenticationServer';
 
 export default function UserLogin(arr) {
-  const [cookies] = useCookies(['redditUser']);
+  const [cookies, setCookies] = useCookies(['redditUser']);
+
+  redditCookie(setCookies);
   const userName = (cookies.redditUser?.userName);
   return arr?.indexOf(userName) !== -1;
 }
