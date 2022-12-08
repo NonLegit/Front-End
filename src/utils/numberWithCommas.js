eb31be11e51f6a283a25a760e1206f430b37313d
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-08 14:09:02 +0200

    fix status

--- src/utils/numberWithCommas.js
+++ src/utils/numberWithCommas.js
@@ -7,6 +7,6 @@
  */
 
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 export default numberWithCommas;
