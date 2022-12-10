0c39d00a2f928aceae47becab1c0a7955644376a
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2022-12-10 02:45:59 +0200

    feat: colored flairs in create post

--- src/components/CreatePost/CreatePostContainer/CreatePostForm/PostTags/Flairs/Flairs.jsx
+++ src/components/CreatePost/CreatePostContainer/CreatePostForm/PostTags/Flairs/Flairs.jsx
@@ -1,7 +1,13 @@
 import {
-  Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, IconButton, Radio, RadioGroup,
+  DialogActions, DialogContent, DialogTitle, RadioGroup,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+import {
+  ApplyFlairButton,
+  CloseButton, CustomDialog, Flair, FlairFormControlLabel, FlairRadio, FlairsContainer, SelectedFlair, Title,
+} from './styles';
+import RedditButton from '../../../../../RedditButton/RedditButton';
 
 function Flairs(props) {
   // props
@@ -9,53 +15,101 @@ function Flairs(props) {
     open, handleOpenFlairs, setFlair, flairs,
   } = props;
 
+  // refs
+  const [currentFlair, setCurrentFlair] = useState(null);
+
   // handlers
   const handleChangeFlair = (event) => {
-    setFlair(event.target.value);
+    setCurrentFlair(event.target.value);
   };
-
+  const handleClearFlairs = () => {
+    setFlair(null);
+  };
+  const handleApplyFlair = () => {
+    setFlair(currentFlair);
+    handleOpenFlairs(false);
+  };
   return (
-    <Dialog open={open}>
+    <CustomDialog open={open}>
       <DialogTitle>
-        Subscribe
-        <IconButton
-          color="primary"
+        <Title>
+          Select r/Egypt flair
+        </Title>
+        <CloseButton
+          disableRipple
+          color="third"
           onClick={() => {
             handleOpenFlairs(false);
             console.log('hiiiiii');
           }}
         >
           <CloseIcon />
-        </IconButton>
+        </CloseButton>
 
       </DialogTitle>
-      <DialogContent>
-        {flairs && (
-        <RadioGroup
-          onChange={handleChangeFlair}
-        >
-          {flairs.map((flair) => {
-            const {
-              _id: id, text, backgroundColor, textColor,
-            } = flair;
-            console.log(backgroundColor, textColor);
-            return (
-              <FormControlLabel
-                value={id}
-                control={<Radio />}
-                label={text}
-                key={id}
-              />
-            );
-          })}
-        </RadioGroup>
-        )}
+      <DialogContent dividers>
+        <SelectedFlair>
+          No flair selected
+        </SelectedFlair>
+        <FlairsContainer>
+          {flairs && (
+          <RadioGroup
+            onChange={handleChangeFlair}
+          >
+            {flairs.map((flair) => {
+              const {
+                _id: id, text, backgroundColor, textColor,
+              } = flair;
+              // console.log(backgroundColor, textColor);
+              // console.log(flairsRef?.current?.value);
+              return (
+                <FlairFormControlLabel
+                  value={id}
+                  selected={currentFlair === id}
+                  control={(
+                    <FlairRadio
+                      disableRipple
+                      size="small"
+                    />
+)}
+                  label={(
+                    <Flair
+                      backgroundColor={backgroundColor}
+                      color={textColor}
+                    >
+                      {text}
+                    </Flair>
+)}
+                  key={id}
+                />
+              );
+            })}
+          </RadioGroup>
+          )}
+        </FlairsContainer>
       </DialogContent>
       <DialogActions>
-        <Button>Cancel</Button>
-        <Button>Subscribe</Button>
+        <RedditButton
+          variant="outlined"
+          padding="3px 16px"
+          fontSize={14}
+          fontWeight="bold"
+          type="submit"
+          onClick={handleClearFlairs}
+        >
+          clear
+        </RedditButton>
+        <ApplyFlairButton
+          variant="contained"
+          type="submit"
+          onClick={handleApplyFlair}
+          disabled={!currentFlair}
+          data-testid="post button"
+        >
+          apply
+        </ApplyFlairButton>
       </DialogActions>
-    </Dialog>
+    </CustomDialog>
   );
 }
 
---
+++ src/components/CreatePost/CreatePostContainer/CreatePostForm/PostTags/Flairs/styles.js
@@ -0,0 +1,89 @@
+import {
+  Dialog, FormControlLabel, IconButton, Radio,
+} from '@mui/material';
+import { styled } from '@mui/system';
+import RedditButton from '../../../../../RedditButton/RedditButton';
+
+export const CustomDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiBackdrop-root': {
+    backgroundColor: 'transparent',
+  },
+  '& .MuiDialog-paper': {
+    height: 558,
+    width: 360,
+    marginTop: 110,
+    [theme.breakpoints.down('sm')]: {
+      width: 200,
+      margin: '5px 10px',
+    },
+  },
+  '& .MuiDialogTitle-root': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  },
+  '& .MuiDialogContent-root': {
+    padding: 0,
+  },
+}));
+
+export const Title = styled('div')(() => ({
+  fontSize: 16,
+  fontWeight: 500,
+}));
+
+export const CloseButton = styled(IconButton)(() => ({
+  padding: 0,
+}));
+
+export const SelectedFlair = styled('div')(() => ({
+  fontWeight: 400,
+  fontSize: 12,
+  padding: '22px 16px',
+}));
+
+export const FlairsContainer = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  height: 348,
+  overflowY: 'auto',
+  padding: '0 2px',
+  backgroundColor: '#f6f7f8',
+}));
+
+export const Flair = styled('div')(({ color, backgroundColor }) => ({
+  color,
+  backgroundColor,
+  padding: '0 4px',
+  width: 'fit-content',
+  borderRadius: 2,
+  fontSize: 12,
+  margin: '5px 0px',
+}));
+
+export const FlairRadio = styled(Radio)(() => ({
+  marginRight: 6,
+  padding: 0,
+  paddingLeft: 10,
+}));
+
+export const FlairFormControlLabel = styled(FormControlLabel)(({ selected }) => ({
+  '&:hover': {
+    backgroundColor: '#24a0ed',
+  },
+  margin: 0,
+  backgroundColor: selected ? '#24a0ed' : 'inherit',
+}));
+
+export const ApplyFlairButton = styled(RedditButton)(() => ({
+  padding: '3px 16px',
+  fontSize: 14,
+  fontWeight: 'bold',
+  '&.Mui-disabled': {
+    cursor: 'not-allowed',
+    pointerEvents: 'auto',
+    backgroundColor: '#8D8D8D',
+    color: '#ffffff80',
+  },
+}));
