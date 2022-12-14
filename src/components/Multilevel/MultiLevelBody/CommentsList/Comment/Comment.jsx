39f7f95d204484e83a1f846f4a39c3ed07c27ef7
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-14 17:53:05 +0200

    more Replies on Multi Levels Bug Solved

--- src/components/Multilevel/MultiLevelBody/CommentsList/Comment/Comment.jsx
+++ src/components/Multilevel/MultiLevelBody/CommentsList/Comment/Comment.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // MUI Components
 import OpenInFullRoundedIcon from '@mui/icons-material/OpenInFullRounded';
@@ -19,21 +19,35 @@ import {
 } from './styles';
 import { MoreCommentsLink } from '../styles';
 
+// Server
+import { getMoreChildren } from '../commentsListServer';
+
 function Comment(props) {
   const {
-    comment, src, depth, level, lastChild, remainingSiblings, loadMoreComments,
+    comment, src, depth, level, lastChild, remainingSiblings, loadMoreRepliesParentFun,
   } = props;
 
   // Constants
   const authorProfilelink = `./user/${comment?.author}`;
-  const replies = (comment) ? comment.replies : [];
+  // const replies = (comment) ? comment.replies : [];
 
   // state
   const [collpase, setCollapse] = useState(false);
+  const [replies, setReplies] = useState(comment.replies);
+
+  useEffect(() => {
+    // setReplies(repliesProp);
+  }, []);
 
   // Functions
   const toggleComment = () => {
     setCollapse(!collpase);
+  };
+
+  // The parent Comment refreshes his replies
+  // new Replies=>Array of strings of new repleis to be added to this replies
+  const loadMoreReplies = () => {
+    getMoreChildren(replies[replies.length - 2]?.children, replies, setReplies);
   };
 
   return (
@@ -65,7 +79,7 @@ function Comment(props) {
                 {/* Loop Over All array of Replies on This Comment */}
                 {replies?.map((reply, i) => {
                   if (i === replies.length - 1) { return null; }
-                  return (<Comment key={reply?._id} comment={reply} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={depth + 1} level={i} lastChild={i === replies.length - 2} remainingSiblings={replies[replies.length - 1]?.count} loadMoreComments={loadMoreComments} />);
+                  return (<Comment key={reply?._id} comment={reply} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={depth + 1} level={i} lastChild={i === replies.length - 2} remainingSiblings={replies[replies.length - 1]?.count} loadMoreRepliesParentFun={loadMoreReplies} />);
                 })}
                 {/* {morereplies ? <p>Continue Thread</p> : null} */}
               </>
@@ -74,7 +88,8 @@ function Comment(props) {
       </CommentContainer>
 
       {lastChild && remainingSiblings > 0 ? (
-        <MoreCommentsLink onClick={loadMoreComments}>
+        // <MoreCommentsLink onClick={loadMoreComments}>
+        <MoreCommentsLink onClick={loadMoreRepliesParentFun}>
           {/* Only the First 10 Comments */}
           {remainingSiblings > 10 ? 10 : remainingSiblings}
           {' '}
--- src/components/Multilevel/MultiLevelBody/CommentsList/CommentsList.jsx
+++ src/components/Multilevel/MultiLevelBody/CommentsList/CommentsList.jsx
@@ -57,7 +57,8 @@ function CommentsList() {
           {/* Loop over   All array of comments on the post */}
           {comments?.map((comment, i) => {
             if (i === comments.length - 1) { return null; }
-            return (<Comment key={comment?._id} comment={comment} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={0} level={i} lastChild={i === comments.length - 2} remainingSiblings={comments[comments.length - 1]?.count} loadMoreComments={loadMoreComments} />);
+            return (<Comment key={comment?._id} comment={comment} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={0} level={i} lastChild={i === comments.length - 2} remainingSiblings={comments[comments.length - 1]?.count} loadMoreRepliesParentFun={loadMoreComments} />);
+            // loadMoreRepliesParentFun=this fucntion
           })}
         </MultilevelPostCommentsConatiner>
       )
