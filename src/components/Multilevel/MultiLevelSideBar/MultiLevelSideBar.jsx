6b81ceba66a07ad0cc9295965a0eab8a940f4d2c
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-14 03:21:42 +0200

    Side Bar For Subreddit Post is Done

--- src/components/Multilevel/MultiLevelSideBar/MultiLevelSideBar.jsx
+++ src/components/Multilevel/MultiLevelSideBar/MultiLevelSideBar.jsx
@@ -1,7 +1,9 @@
 import { useCookies } from 'react-cookie';
+import { useState, useEffect } from 'react';
 
 // Components
 import SideBar from '../../SideBar/SideBar';
+import SideBarSubreddit from '../../SubReddit/SideBar/SideBar';
 import UserInfo from '../../MainProfile/Profile/MainContent/Sidebar/UserInfo/UserInfo';
 import OtherProfileUserInfo from '../../MainProfile/OtherProfile/OtherProfileMainContent/OtherProfileSidebar/OtherProfileUserInfo/OtherProfileUserInfo';
 import BackToTop from '../../BackToTop/BackToTop';
@@ -10,6 +12,9 @@ import BackToTop from '../../BackToTop/BackToTop';
 import { usePostContext } from '../../../contexts/PostContext';
 import UserProvider from '../../../contexts/UserProvider';
 
+// Server
+import getSubredditAllData from '../../SubReddit/SubrridetDataServer';
+
 function MultiLevelSideBar() {
   // Context
   const { post } = usePostContext();
@@ -17,25 +22,49 @@ function MultiLevelSideBar() {
   // cookies
   const [cookies] = useCookies(['redditUser']);
 
+  // useState
+  const [sunreddit, setSubreddit] = useState(null);
+
+  // Fetch Subreddit
+  // eslint-disable-next-line no-unused-vars
+  const [SubredditData, dataError, statusCode] = getSubredditAllData(post?.owner?.name);
+
+  useEffect(() => {
+    setSubreddit(SubredditData);
+  }, [SubredditData]);
+
   return (
-    <SideBar>
-      Side Bar
-      {post?.ownerType === 'Subreddit' ? <h1>Subreddit</h1>
-        : (post?.ownerType === 'User'
-          ? ((cookies?.redditUser?.id === post?.owner?._id)
-            ? (
-              <UserProvider name={post?.owner?.name}>
-                <UserInfo />
-              </UserProvider>
-            )
-            : (
-              <UserProvider name={post?.owner?.name}>
-                <OtherProfileUserInfo />
-              </UserProvider>
-            )
-          ) : null)}
-      <BackToTop />
-    </SideBar>
+    post?.ownerType === 'Subreddit' ? (
+      <SideBarSubreddit
+        disc={sunreddit?.description}
+        topics={sunreddit?.topics}
+        Name={sunreddit?.name}
+        primaryTopic={sunreddit?.primaryTopic}
+        createdAt={sunreddit?.createdAt}
+        moderatoesName={sunreddit?.moderators}
+        username={cookies?.redditUser?.username}
+        members={sunreddit?.membersCount}
+        postCard
+      />
+    )
+      : (
+        <SideBar>
+          {(post?.ownerType === 'User'
+            ? ((cookies?.redditUser?.id === post?.owner?._id)
+              ? (
+                <UserProvider name={post?.owner?.name}>
+                  <UserInfo />
+                </UserProvider>
+              )
+              : (
+                <UserProvider name={post?.owner?.name}>
+                  <OtherProfileUserInfo />
+                </UserProvider>
+              )
+            ) : null)}
+          <BackToTop />
+        </SideBar>
+      )
   );
 }
 
