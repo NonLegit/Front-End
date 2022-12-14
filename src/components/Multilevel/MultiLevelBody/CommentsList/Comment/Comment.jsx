1b80cbe1b43de0ddff3b5936e1538a761c5d8859
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-14 13:29:35 +0200

    Modified Attribute text in Json server and in comment Component

--- src/components/Multilevel/MultiLevelBody/CommentsList/Comment/Comment.jsx
+++ src/components/Multilevel/MultiLevelBody/CommentsList/Comment/Comment.jsx
@@ -64,7 +64,7 @@ function Comment(props) {
           {collpase ? null
             : (
               <>
-                <CommentText>{comment?.post}</CommentText>
+                <CommentText>{comment?.text}</CommentText>
                 <CommentActions />
                 {replies?.map((reply, i) => <Comment key={reply?._id} comment={reply} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={depth + 1} level={i} />)}
                 {/* {morereplies ? <p>Continue Thread</p> : null} */}
