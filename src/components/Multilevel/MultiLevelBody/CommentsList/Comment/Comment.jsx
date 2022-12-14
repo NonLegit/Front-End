8722bd6cf2115e93e012659a5e90a0094d88d9f5
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-14 17:09:11 +0200

    Problem Of Inner Level

--- src/components/Multilevel/MultiLevelBody/CommentsList/Comment/Comment.jsx
+++ src/components/Multilevel/MultiLevelBody/CommentsList/Comment/Comment.jsx
@@ -62,7 +62,11 @@ function Comment(props) {
               <>
                 <CommentText>{comment?.text}</CommentText>
                 <CommentActions />
-                {replies?.map((reply, i) => <Comment key={reply?._id} comment={reply} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={depth + 1} level={i} />)}
+                {/* Loop Over All array of Replies on This Comment */}
+                {replies?.map((reply, i) => {
+                  if (i === replies.length - 1) { return null; }
+                  return (<Comment key={reply?._id} comment={reply} src="https://styles.redditmedia.com/t5_74w4tr/styles/profileIcon_9or0sb8dtc5a1.jpeg?width=256&height=256&crop=256:256,smart&s=2a8b7dc794b00e51a6b9f423da2204a999136ecb" depth={depth + 1} level={i} lastChild={i === replies.length - 2} remainingSiblings={replies[replies.length - 1]?.count} loadMoreComments={loadMoreComments} />);
+                })}
                 {/* {morereplies ? <p>Continue Thread</p> : null} */}
               </>
             )}
