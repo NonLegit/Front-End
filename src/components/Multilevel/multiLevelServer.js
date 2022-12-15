85b26109a7f412e0f5ae48097f870696d524b4ff
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-15 19:51:01 +0200

    Modified EndPoint of posts

--- src/components/Multilevel/multiLevelServer.js
+++ src/components/Multilevel/multiLevelServer.js
@@ -3,11 +3,11 @@ import axios from '../../services/instance';
 
 export const getPost = (postID, setPost) => {
   axios.get(`/posts/${postID}`).then((response) => {
-    console.log(response);
+    console.log('Response of get post', response);
     // 200
     if (response.status === 200 || response.status === 200) {
       console.log('Post is get :)', postID);
-      setPost(response.data.post);
+      setPost(response.data);
     }
     return {};
   }).catch((error) => {
