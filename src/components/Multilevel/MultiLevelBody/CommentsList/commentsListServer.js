50c434ea9e67c4b34af1ab07cd1e7acbf41f2a54
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-15 23:15:08 +0200

    Added Reply on Comment endpoint

--- src/components/Multilevel/MultiLevelBody/CommentsList/commentsListServer.js
+++ src/components/Multilevel/MultiLevelBody/CommentsList/commentsListServer.js
@@ -26,7 +26,7 @@ export const getComments = (props, setComments) => {
       },
     },
   ).then((response) => {
-    console.log(response);
+    console.log('/comments/comment_tree', response);
     // 200
     if (response.status === 200 || response.status === 304) {
       console.log('Commnets for post ', postID, 'are got :)');
@@ -75,5 +75,29 @@ export const getMoreChildren = (children, comments, setComments) => {
     // 400 Error
     // Error message:error.response.data.message
     console.log(error);
+  });
+};
+
+export const saveComment = async (parentID, parentType, comment) => {
+  await axios.post(
+    '/comments',
+    {
+      parent: `${parentID}`,
+      parentType,
+      text: comment,
+    },
+  ).then((response) => {
+    console.log(response);
+    // 201
+    if (response.status === 201 || response.status === 200 || response.status === 304) {
+      console.log('Comment Saved Sucessfully :)', comment, 'on Post', parentID);
+      return true;
+    }
+    return false;
+  }).catch((error) => {
+    console.log(error);
+    // 400
+    // 404
+    return false;
   });
 };
