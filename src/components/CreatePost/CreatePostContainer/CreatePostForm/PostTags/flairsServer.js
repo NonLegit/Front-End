43f19a309de2bd3dfb918d66b97416e582880b07
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-15 19:26:21 +0200

    fix flairs domain

--- src/components/CreatePost/CreatePostContainer/CreatePostForm/PostTags/flairsServer.js
+++ src/components/CreatePost/CreatePostContainer/CreatePostForm/PostTags/flairsServer.js
@@ -3,7 +3,7 @@ import conditionalUseFetch from '../../../../../hooks/conditionalUseFetch';
 const flairsServer = (subreddit) => {
   // console.log('subreddit', subreddit);
   console.log('el subreddit', subreddit);
-  const [data, error, statusCode] = conditionalUseFetch(`/subreddits/${subreddit}/flairs`, subreddit);
+  const [data, error, statusCode] = conditionalUseFetch(`/subreddits/${subreddit}/flair`, subreddit);
   const flairs = data?.data;
   // console.log(data);
   // console.log('el gommaaaaa', flairs, statusCode, error);
