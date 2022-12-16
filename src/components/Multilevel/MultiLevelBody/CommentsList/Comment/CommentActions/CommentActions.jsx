9c478c3681a5490b9132450a7b247730d786c107
Nour Almulhem <nouralmulhem@gmail.com>
2022-12-16 04:32:38 +0200

    solve conflicts

--- src/components/Multilevel/MultiLevelBody/CommentsList/Comment/CommentActions/CommentActions.jsx
+++ src/components/Multilevel/MultiLevelBody/CommentsList/Comment/CommentActions/CommentActions.jsx
@@ -25,7 +25,7 @@ import { usePostContext } from '../../../../../../contexts/PostContext';
 
 // styles
 import { CommentActionsContainer } from './styles';
-import { ElementBox, FooterText } from '../../../../../MainProfile/OtherProfile/OtherProfileMainContent/OtherProfileContent/OtherProfilePosts/OtherProfilePostFooter/styles';
+import { ElementBox, FooterText } from '../../../../../MainProfile/Posts/PostFooter/styles';
 import { SaveButton } from '../../../MultiLevelPostContent/styles';
 
 // Server
