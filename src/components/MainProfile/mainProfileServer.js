56b764555b7e2a2ed3054286199ddead95e2ae85
Nour Almulhem <nouralmulhem@gmail.com>
2022-12-17 22:32:33 +0200

    follow request and search

--- src/components/MainProfile/mainProfileServer.js
+++ src/components/MainProfile/mainProfileServer.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import axios from '../../services/instance';
 
 import useFetch from '../../hooks/useFetch';
 
@@ -14,3 +15,39 @@ const UserInfoServer = (username) => {
 };
 
 export default UserInfoServer;
+
+/**
+ * follow a user
+ *
+ * @property {string} username - username of user to be followed
+ * @returns {object} provided response through backend
+ */
+
+export const followRequest = (username, isFollowed, handleNotBlocked) => {
+  const [data, setData] = useState(null);
+
+  const isFirstRun = useRef(true);
+  useEffect(() => {
+    if (isFollowed === undefined) {
+      return;
+    }
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+    axios.post(`/users/${username}/${isFollowed ? 'follow' : 'unfollow'}`, {
+
+    }).then((response) => {
+      // console.log(response);
+      if (response.status === 200 || response.status === 201) {
+        handleNotBlocked();
+      } else if (response.status === 401) {
+        window.location.pathname = 'login';
+      }
+      setData(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, [username, isFollowed]);
+  return [data];
+};
