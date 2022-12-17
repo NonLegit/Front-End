5d67f5dfec8b0399ebd11e61e60ff578121fe628
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-12-17 09:49:17 +0200

    add router of explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,12 +1,11 @@
 import { useParams } from 'react-router-dom';
 import ExploreMainPage from '../components/Explore/ExploreMainPage/ExploreMainPage';
-// import ExploreHeader from '../components/Explore/ExploreHeader/ExploreHeader';
 
-function Moderation() {
+function Explore() {
   const { title } = useParams();
   return (
     <ExploreMainPage title={title} />
   );
 }
 
-export default Moderation;
+export default Explore;
