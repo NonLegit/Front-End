641cf45555d341cb3dc63930a30b3bde1d43e22b
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-23 19:21:26 +0200

    add unit test to messages

--- src/components/Messages/MessageFooter/MessageFooter.test.jsx
+++ src/components/Messages/MessageFooter/MessageFooter.test.jsx
@@ -2,9 +2,12 @@ import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/system';
+import
+{
+  render, screen,
+} from '@testing-library/react';
 import theme from '../../../styles/theme';
 import MessageFooter from './MessageFooter';
-
 // test snapshot
 test('test snapshot', () => {
   const tree = renderer.create(
@@ -15,4 +18,16 @@ test('test snapshot', () => {
     </Router>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+// render  messages footer
+it('should render  messages footer', () => {
+  window.history.pushState({}, '', '/messages');
+  render(
+    <Router>
+      <ThemeProvider theme={theme}>
+        <MessageFooter />
+      </ThemeProvider>
+    </Router>,
+  );
+  expect(screen.queryByText('REDDIT and the ALIEN Logo are registered trademarks of reddit inc.')).toBeInTheDocument();
 });
--- src/components/Messages/MessageSent/MessageBody/MessageBody.test.jsx
+++ src/components/Messages/MessageSent/MessageBody/MessageBody.test.jsx
@@ -1,18 +1,42 @@
 import '@testing-library/jest-dom';
-import renderer from 'react-test-renderer';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/system';
+
+import ShallowRenderer from 'react-test-renderer/shallow';
+import
+{
+  render, screen,
+} from '@testing-library/react';
 import theme from '../../../../styles/theme';
 import MessageBody from './MessageBody';
 
+const renderer = new ShallowRenderer();
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
 // test snapshot
 test('test snapshot', () => {
-  const tree = renderer.create(
+  const tree = renderer.render(
     <Router>
       <ThemeProvider theme={theme}>
         <MessageBody />
       </ThemeProvider>
     </Router>,
-  ).toJSON();
+  );
   expect(tree).toMatchSnapshot();
+});
+// render unread messages page
+it('should render unread messages page', () => {
+  window.history.pushState({}, '', '/messages');
+  render(
+    <Router>
+      <ThemeProvider theme={theme}>
+        <MessageBody />
+      </ThemeProvider>
+    </Router>,
+  );
+  expect(screen.queryByText('from')).not.toBeInTheDocument();
 });
