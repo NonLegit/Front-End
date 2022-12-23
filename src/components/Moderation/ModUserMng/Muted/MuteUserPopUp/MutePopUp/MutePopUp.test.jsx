e93c550eef1b85f51e18f7127f2f97b95859e62c
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-12-23 15:32:47 +0200

    solve failed tests

--- src/components/Moderation/ModUserMng/Muted/MuteUserPopUp/MutePopUp/MutePopUp.test.jsx
+++ src/components/Moderation/ModUserMng/Muted/MuteUserPopUp/MutePopUp/MutePopUp.test.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import MutePopUp from './MutePopUp';
 import MutedUser from '../../NonEmptyMuted/MutedUser/MutedUser';
 
+const user = { userName: 'fady' };
+const muteInfo = { muteMessage: 'Spammer' };
+
 test('test snapshot', async () => {
-  const tree = renderer.create(<Router><MutedUser><MutePopUp /></MutedUser></Router>).toJSON();
+  const tree = renderer.create(<Router><MutedUser user={user} muteInfo={muteInfo}><MutePopUp /></MutedUser></Router>).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/Moderation/ModUserMng/Muted/MuteUserPopUp/UnmutePopUp/UnmutePopUp.test.jsx
+++ src/components/Moderation/ModUserMng/Muted/MuteUserPopUp/UnmutePopUp/UnmutePopUp.test.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import UnmutePopUp from './UnmutePopUp';
 import MutedUser from '../../NonEmptyMuted/MutedUser/MutedUser';
 
+const user = { userName: 'fady' };
+const muteInfo = { muteMessage: 'Spammer' };
+
 test('test snapshot', async () => {
-  const tree = renderer.create(<Router><MutedUser><UnmutePopUp userName="fady" /></MutedUser></Router>).toJSON();
+  const tree = renderer.create(<Router><MutedUser user={user} muteInfo={muteInfo}><UnmutePopUp userName={user.userName} /></MutedUser></Router>).toJSON();
   expect(tree).toMatchSnapshot();
 });
