2d9f6834dd5ae74942ae0d76544906fd634ecd52
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-23 17:49:41 +0200

    add DraggableMedia

--- src/components/CreatePost/CreatePostContainer/CreatePostForm/PostMedia/DraggableMedia/DraggableMedia.test.jsx
+++ src/components/CreatePost/CreatePostContainer/CreatePostForm/PostMedia/DraggableMedia/DraggableMedia.test.jsx
@@ -1,27 +1,33 @@
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 import DraggableMedia from './DraggableMedia';
 
 describe(DraggableMedia, () => {
-  jest.mock('react-dnd', () => ({
-    ...jest.requireActual('react-dnd'), // use actual for all non-hook parts
-    useDrag: () => ([{ isDragging: 'isDragging' }, 'drag']),
-    useDrop: () => ([{ isOver: 'isOver' }, 'drop']),
-  }));
+  // jest.mock('react-dnd', () => ({
+  //   ...jest.requireActual('react-dnd'), // use actual for all non-hook parts
+  //   useDrag: (p) => ([{ isDragging: p }, 'drag']),
+  //   useDrop: (x) => ([{ isOver: x }, 'drop']),
+  // }));
   it('DraggableMedia renders correctly', () => {
     const renderer = new ShallowRenderer();
     const tree = renderer
-      .render(<DraggableMedia
-        media={
+      .render(
+        <DndProvider backend={HTML5Backend}>
+          <DraggableMedia
+            media={
           {
             src: 'src',
           }
         }
-        id={1}
-        mediaSwap={() => (null)}
-        activeMediaFile={0}
-        setActiveMediaFile={() => (null)}
-        mediaDelete={() => (null)}
-      />);
+            id={1}
+            mediaSwap={() => (null)}
+            activeMediaFile={0}
+            setActiveMediaFile={() => (null)}
+            mediaDelete={() => (null)}
+          />
+        </DndProvider>,
+      );
     expect(tree).toMatchSnapshot();
   });
 });
--- src/components/CreatePost/CreatePostContainer/CreatePostForm/PostMedia/PostMediaDetails/PostMediaDetails.test.jsx
+++ src/components/CreatePost/CreatePostContainer/CreatePostForm/PostMedia/PostMediaDetails/PostMediaDetails.test.jsx
@@ -1,16 +1,22 @@
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { ThemeProvider } from '@mui/system';
 import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import PostMediaDetails from './PostMediaDetails';
 import theme from '../../../../../../styles/theme';
 
+jest.mock('react-dnd', () => ({
+  ...jest.requireActual('react-dnd'), // use actual for all non-hook parts
+  useDrag: (p) => ([{ isDragging: p }, 'drag']),
+  useDrop: (x) => ([{ isOver: x }, 'drop']),
+}));
 describe(PostMediaDetails, () => {
   it('PostMediaDetails renders correctly', () => {
     const renderer = new ShallowRenderer();
     const tree = renderer
       .render(
         <ThemeProvider theme={theme}>
-          <DndProvider>
+          <DndProvider backend={HTML5Backend}>
             <PostMediaDetails
               mediaFile={
         {
