0110bb2ebff7b0685b5cc723ddce7423597de885
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-12-23 17:16:02 +0200

    add unit testing

--- src/components/MainNavBar/Search/Search.test.jsx
+++ src/components/MainNavBar/Search/Search.test.jsx
@@ -1,6 +1,12 @@
 import renderer from 'react-test-renderer';
 import SearchField from './Search';
 
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
 test('test snapshot', async () => {
   const tree = renderer.create(<SearchField />).toJSON();
   expect(tree).toMatchSnapshot();
