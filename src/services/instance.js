7fe4b912518de551706e241df7c81cf22edcf745
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-24 10:51:14 +0200

    update instance

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-// const { REACT_APP_ENV, REACT_APP_PROXY_DEVELOPMENT, REACT_APP_PROXY_PRODUCTION } = process.env;
+const { REACT_APP_ENV, REACT_APP_PROXY_DEVELOPMENT, REACT_APP_PROXY_PRODUCTION } = process.env;
 const instance = axios.create({
-  // baseURL: REACT_APP_ENV === 'development' ? REACT_APP_PROXY_DEVELOPMENT : REACT_APP_PROXY_PRODUCTION,
-  baseURL: 'http://localhost:8000',
+  baseURL: REACT_APP_ENV === 'development' ? REACT_APP_PROXY_DEVELOPMENT : REACT_APP_PROXY_PRODUCTION,
   withCredentials: true,
   crossDomain: true,
   headers: {
