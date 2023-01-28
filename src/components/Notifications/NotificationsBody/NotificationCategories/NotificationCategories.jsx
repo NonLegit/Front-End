2b51024fc135d5be47bdc21356f0f7bfb2ad9686
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-01-28 02:12:50 +0200

    fix notification roter

--- src/components/Notifications/NotificationsBody/NotificationCategories/NotificationCategories.jsx
+++ src/components/Notifications/NotificationsBody/NotificationCategories/NotificationCategories.jsx
@@ -105,7 +105,7 @@ function NotificationCategories({ NavBar }) {
     return '';
   };
   const handleLink = (ele) => {
-    const follow = (ele?.followedSubreddit) ? `/r/${ele?.followedSubreddit.fixedName}` : `/user/${cookies?.redditUser.userName}`;
+    const follow = (ele?.followedSubreddit) ? `/subreddit/${ele?.followedSubreddit.fixedName}` : `/user/${cookies?.redditUser.userName}`;
     if (!ele.seen) { notificationMarkRead(ele._id); }
     if (ele?.type === 'follow') {
       navigate(`/user/${ele.followerUser.userName}`);
